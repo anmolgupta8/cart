3143eadde253e43298c10bfcf161fef7d7ac9153
Anmol Gupta <anmolgupta081202@gmail.com>
2023-02-26 02:03:35 +0530

    Inc/Dec Qty

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,35 +1,91 @@
-import React from 'react';
+import React from "react";
 
 class CartItem extends React.Component {
-    render(){
-        return (
-            <div className='cart-item'>
-                <div className='left-block'>
-                    <img style={styles.image} />
-                </div>
-                <div className='right-block'>
-                    <div style = {styles.heading} >Phone</div>
-                    <div>Rs 10999</div>
-                    <div>Qty : 1</div>
-                    <div className='cart-item-actions'>
-                        { /* This is for buttons */ }
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  constructor() {
+    super();
+    this.state = {
+      price: 10999,
+      title: "Phone",
+      qty: 1,
+      img: "",
+    };
+    // this.increaseOuantity = this.increaseOuantity.bind(this);
+  }
+  increaseOuantity = () => {
+    // Arrow functions automatically binds
+    // this.state.qty++; Will not work
+
+    // setState() method 1 :
+    // this.setState({
+    //   qty: this.state.qty + 1,
+    // });
+
+    // setState() method 2 :   If previous state is required, use this method otherwise use method 1
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty + 1,
+      };
+    });
+  };
+
+  decreaseOuantity = () => {
+    const {qty} = this.state;
+    if(qty==0) return;
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty - 1,
+      };
+    });
+  };
+
+  render() {
+    const { price, title, qty } = this.state;
+    return (
+      <div className="cart-item">
+        <div className="left-block">
+          <img style={styles.image} />
+        </div>
+        <div className="right-block">
+          <div style={styles.heading}>{title}</div>
+          <div>Rs {price}</div>
+          <div>Qty : {qty}</div>
+          <div className="cart-item-actions">
+            {/* This is for buttons */}
+            <img
+              alt="increase"
+              className="action-icons"
+              src="https://as2.ftcdn.net/v2/jpg/01/07/62/07/1000_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg"
+              //   onClick = {this.increaseOuantity.bind(this)}
+              onClick={this.increaseOuantity}
+            />
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg"
+              onClick={this.decreaseOuantity}
+            />
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const styles = {
-    image : {
-      height : 110,
-      width : 110,
-      borderRadius : 4,
-      backgroundColor : 'lightGrey'
-    },
-    heading : {
-        fontSize : 30
-    }
-}
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    backgroundColor: "lightGrey",
+  },
+  heading: {
+    fontSize: 30,
+  },
+};
 
 export default CartItem;
